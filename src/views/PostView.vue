abaafb9a926d7176f20274df028ac2076dee4acf
kasidatep <bank@kasidate.me>
2023-11-30 20:23:18 +0700

    Gossip App Init

---
+++ src/views/PostView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, onMounted, watch } from 'vue'
+import { useRoute } from "vue-router"
+import { collection, query, where, getDocs, onSnapshot } from "firebase/firestore"
+import db from "../firebase/init.js"
+
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
+
